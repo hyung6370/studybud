62aa34d8fa838cc908c27e957394412735bafece
emptyhead <korea6370@naver.com>
2022-08-25 23:27:18 +0900

    first commit

---
+++ studybud/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room)
+]
