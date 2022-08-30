dfbfe35c80b0d7fcd8fdc4b7dfec88d7d8562cd4
emptyhead <korea6370@naver.com>
2022-08-30 14:55:42 +0900

    css

--- studybud/settings.py
+++ studybud/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILS_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
